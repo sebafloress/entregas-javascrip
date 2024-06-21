98a7cec650275838d6658c0c242093c7345ec6a6
sebastian <seba10flores@icloud.com>
2024-06-20 23:00:13 -0300

    tercera pre entrega

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
-
-
 const productos = [
-    { id: 1, nombre: 'pelota adidas afa22 pro', precio: 110000, cantidad: 30 },
-    { id: 2, nombre: 'pelota orbita serie A', precio: 130000, cantidad: 20 },
-    { id: 3, nombre: 'canillera kick topper', precio: 13000, cantidad: 43 },
-    { id: 4, nombre: 'botines fútbol nike mercurial superfly 9 elite Fg', precio: 380000, cantidad: 15 },
-    { id: 5, nombre: 'botines predator adidas', precio: 380000, cantidad: 15 },
-    { id: 6, nombre: 'paleta De padel adidas metalbone Hrd 3.2', precio: 527000, cantidad: 20 }
+    { id: 1, nombre: 'Pelota adidas afa22 pro', precio: 110000, cantidad: 30, imagen: '../fotos/botines.jpeg' },
+    { id: 2, nombre: 'Pelota orbita serie A', precio: 130000, cantidad: 20, imagen: '../fotos/botines.jpeg' },
+    { id: 3, nombre: 'Canillera kick topper', precio: 13000, cantidad: 43, imagen: '../fotos/botines.jpeg' },
+    { id: 4, nombre: 'Botines fútbol nike mercurial superfly 9 elite Fg', precio: 380000, cantidad: 15, imagen: '../fotos/botines.jpeg' },
+    { id: 5, nombre: 'Botines predator adidas', precio: 380000, cantidad: 15, imagen: '../fotos/botines.jpeg' },
+    { id: 6, nombre: 'Paleta De padel adidas metalbone Hrd 3.2', precio: 527000, cantidad: 20, imagen: '../fotos/botines.jpeg' }
 ];
 
 const IVA = 0.21;
@@ -14,10 +12,21 @@ let carrito = [];
 let total = 0;
 
 document.addEventListener('DOMContentLoaded', () => {
+    // Cargar carrito desde localStorage
+    const carritoGuardado = localStorage.getItem('carrito');
+    const totalGuardado = localStorage.getItem('total');
+    
+    if (carritoGuardado) {
+        carrito = JSON.parse(carritoGuardado);
+        total = parseFloat(totalGuardado);
+        actualizarCarrito();
+    }
+
     mostrarProductos();
     document.getElementById('filtrar-precio').addEventListener('click', filtrarProductos);
     document.getElementById('buscar-producto').addEventListener('click', buscarProducto);
     document.getElementById('mostrar-resumen').addEventListener('click', mostrarResumen);
+    document.getElementById('reiniciar-compra').addEventListener('click', reiniciarCompra);
 });
 
 function mostrarProductos() {
@@ -25,8 +34,24 @@ function mostrarProductos() {
     productList.innerHTML = '';
     productos.forEach(producto => {
         const li = document.createElement('li');
-        li.textContent = `${producto.nombre} - $${producto.precio}`;
-        li.addEventListener('click', () => agregarAlCarrito(producto));
+        li.classList.add('product-item', `product-item-${producto.id}`);
+        
+        const img = document.createElement('img');
+        img.src = producto.imagen;
+        img.alt = producto.nombre;
+        img.classList.add('product-image');
+
+        const span = document.createElement('span');
+        span.textContent = `${producto.nombre} - $${producto.precio}`;
+        
+        const button = document.createElement('button');
+        button.textContent = 'Agregar al carrito';
+        button.classList.add(`botonAgregarCarrito`);
+        button.addEventListener('click', () => agregarAlCarrito(producto));
+        
+        li.appendChild(img);
+        li.appendChild(span);
+        li.appendChild(button);
         productList.appendChild(li);
     });
 }
@@ -43,6 +68,7 @@ function agregarAlCarrito(productoSeleccionado) {
     }
 
     actualizarCarrito();
+    guardarCarritoEnLocalStorage(); // Actualizar localStorage después de modificar el carrito
 }
 
 function actualizarCarrito() {
@@ -56,31 +82,47 @@ function actualizarCarrito() {
     document.getElementById('total').textContent = `Total: $${total}`;
 }
 
+function guardarCarritoEnLocalStorage() {
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+    localStorage.setItem('total', total.toString());
+}
+
+
 function buscarProducto() {
-    const nombreProducto = prompt('Ingrese el nombre del producto a buscar:').trim().toLowerCase();
+    const nombreProducto = document.getElementById('nombre-producto').value.trim().toLowerCase();
     console.log('Nombre ingresado:', nombreProducto);
 
-    const producto = productos.find(p => p.nombre.toLowerCase().includes(nombreProducto));
+    const productosFiltrados = productos.filter(p => p.nombre.toLowerCase().includes(nombreProducto));
 
-    if (producto) {
-        alert(`Producto encontrado: ${producto.nombre}, Precio: ${producto.precio}, Cantidad: ${producto.cantidad}`);
+    const filteredProductList = document.getElementById('filtrar-productos-list');
+    filteredProductList.innerHTML = '';
+
+    if (productosFiltrados.length > 0) {
+        productosFiltrados.forEach(p => {
+            const li = document.createElement('li');
+            li.textContent = `Producto encontrado: ${p.nombre}, Precio: $${p.precio}, Cantidad: ${p.cantidad}`;
+            filteredProductList.appendChild(li);
+        });
     } else {
-        alert('Producto no encontrado.');
+        filteredProductList.textContent = 'Producto no encontrado.';
     }
 }
 
 function filtrarProductos() {
-    const maxPrecio = parseFloat(prompt('Ingrese el precio máximo:'));
+    const maxPrecio = parseFloat(document.getElementById('max-precio').value) || 0;
     const productosFiltrados = productos.filter(p => p.precio <= maxPrecio);
 
+    const filteredProductList = document.getElementById('filtrar-productos-list');
+    filteredProductList.innerHTML = '';
+
     if (productosFiltrados.length > 0) {
-        let mensaje = 'Productos encontrados:';
         productosFiltrados.forEach(p => {
-            mensaje += `\nNombre: ${p.nombre}, Precio: ${p.precio}, Cantidad: ${p.cantidad}`;
+            const li = document.createElement('li');
+            li.textContent = ` ${p.nombre}, Precio: $${p.precio}, Cantidad: ${p.cantidad}`;
+            filteredProductList.appendChild(li);
         });
-        alert(mensaje);
     } else {
-        alert('No se encontraron productos con ese precio máximo.');
+        filteredProductList.textContent = 'No se encontraron productos con ese precio máximo.';
     }
 }
 
@@ -89,14 +131,14 @@ function calcularIVA(total) {
 }
 
 function mostrarResumen() {
-    let descuento = parseFloat(prompt('Ingrese el porcentaje de descuento:')) || 0;
+    let descuento = parseFloat(document.getElementById('descuento').value) || 0;
     descuento = Math.max(0, Math.min(descuento, 100)); // Asegurarse de que el descuento esté entre 0 y 100
     const montoDescuento = total * (descuento / 100);
     const totalConDescuento = total - montoDescuento;
 
     let resumen = 'Resumen de la compra:\n';
     carrito.forEach(producto => {
-        resumen += `Nombre: ${producto.nombre}, Cantidad: ${producto.cantidad}, Precio Total: $${producto.precio * producto.cantidad}\n`;
+        resumen += `${producto.nombre}, Cantidad: ${producto.cantidad}, Precio Total: $${producto.precio * producto.cantidad}\n`;
     });
 
     const iva = calcularIVA(totalConDescuento);
@@ -110,4 +152,11 @@ function mostrarResumen() {
 
     document.getElementById('resumen-compra').textContent = resumen;
 }
-
+function reiniciarCompra() {
+    carrito = [];
+    total = 0;
+    actualizarCarrito();
+    guardarCarritoEnLocalStorage();
+    document.getElementById('resumen-compra').textContent = '';
+    document.getElementById('descuento').value = 0;
+}
